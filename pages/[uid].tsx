fc00faa4a609f9b0eb9a0b86a2ab4f0629f4b0d0
Daniel <dpadayao40@gmail.com>
2022-08-16 19:53:13 +0800

    indicator update

--- pages/[uid].tsx
+++ pages/[uid].tsx
@@ -20,8 +20,9 @@ const Person: PageWithLayout = () => {
 
     return (
         <div className={styles.container}>
-            <div className=' my-2 bg-black/5 rounded-md w-full h-full grid place-content-center font-thin'>
-                {query.uid?.toString().toUpperCase()}
+            <div className=' my-2 bg-black/5 rounded-md w-full h-full grid place-content-center font-thin px-6'>
+                <h3>{query.uid?.toString().toUpperCase()}</h3>
+                <p className='text-xs max-w-xs text-neutral-500'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim facere nam molestiae tenetur nihil!</p>
             </div>
         </div>
     )
